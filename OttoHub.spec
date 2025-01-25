b6ceeef1f2d6d703240e22a3993b2ef42c54fea4
Allen Etcetera <allencr1998@gmail.com>
2025-01-25 15:31:29 -0700

    Added executable and list of requirements.

---
+++ OttoHub.spec
@@ -0,0 +1,38 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['/home/allen/Documents/BYUI Programming/Senior Project A/scripts/OttoHub.py'],
+    pathex=[],
+    binaries=[],
+    datas=[('/home/allen/Documents/BYUI Programming/Senior Project A/scripts', './scripts')],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+    optimize=0,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name='OttoHub',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=True,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+)
--- scripts/OttoHub.py
+++ scripts/OttoHub.py
@@ -1,13 +1,15 @@
 import os
 import shutil
+import sys
 import tkinter as tk
 from tkinter import messagebox, simpledialog, ttk
 import pyautogui
 import subprocess
 import threading
 import time
+import tkcalendar
 from plyer import notification
-from playsound import playsound
+import simpleaudio as sa
 from pynput import keyboard
 from pathlib import Path
 from collections import Counter
@@ -37,13 +39,17 @@ def toggle_hourpass():
             while notif_on:
                 time.sleep(3600) #3600 is 1 hour
                 if notif_on:
-                    playsound('hourpass.mp3')
+                    
+                    str(BASE_DIR / 'scripts' / 'hourpass.wav')
                     notification.notify(
                         title='Otto Hour',
                         message='An hour has passed.',
                         timeout=10  # seconds
                     )
         threading.Thread(target=notify,daemon=True).start()
+
+# Base directory of the executable or script
+BASE_DIR = Path(sys._MEIPASS) if hasattr(sys, "_MEIPASS") else Path(__file__).resolve().parent
 
 # Create window
 def create_window():
@@ -52,25 +58,25 @@ def create_window():
     rootHub = tk.Tk()
     rootHub.title("Otto Work Optimizer")
     # Otto Input button
-    input_button = tk.Button(rootHub, text="Input", command=lambda:subprocess.Popen(['python', './scripts/OttoInput.py']))
+    input_button = tk.Button(rootHub, text="Input", command=lambda:subprocess.Popen(['python', str(BASE_DIR / 'scripts' / 'OttoInput.py')]))
     input_button.grid(row=0,column=0,pady=5,padx=5)
     # Otto List button
-    input_button = tk.Button(rootHub, text="List", command=lambda:subprocess.Popen(['python', './scripts/OttoList.py']))
+    input_button = tk.Button(rootHub, text="List", command=lambda:subprocess.Popen(['python', str(BASE_DIR / 'scripts' / 'OttoList.py')]))
     input_button.grid(row=0,column=1,pady=5,padx=5)
     # Otto Math button
-    input_button = tk.Button(rootHub, text="Math", command=lambda:subprocess.Popen(['python', './scripts/OttoMath.py']))
+    input_button = tk.Button(rootHub, text="Math", command=lambda:subprocess.Popen(['python', str(BASE_DIR / 'scripts' / 'OttoMath.py')]))
     input_button.grid(row=0,column=2,pady=5,padx=5)
     # Otto Note button
-    input_button = tk.Button(rootHub, text="Notes", command=lambda:subprocess.Popen(['python', './scripts/OttoNote.py']))
+    input_button = tk.Button(rootHub, text="Notes", command=lambda:subprocess.Popen(['python', str(BASE_DIR / 'scripts' / 'OttoNote.py')]))
     input_button.grid(row=1,column=0,pady=5,padx=5)
     # Otto Overwrite button
-    input_button = tk.Button(rootHub, text="Overwrite", command=lambda:subprocess.Popen(['python', './scripts/OttoOverwrite.py']))
+    input_button = tk.Button(rootHub, text="Overwrite", command=lambda:subprocess.Popen(['python', str(BASE_DIR / 'scripts' / 'OttoOverwrite.py')]))
     input_button.grid(row=1,column=1,pady=5,padx=5)
     # Otto Remind button
-    input_button = tk.Button(rootHub, text="Remind", command=lambda:subprocess.Popen(['python', './scripts/OttoRemind.py']))
+    input_button = tk.Button(rootHub, text="Remind", command=lambda:subprocess.Popen(['python', str(BASE_DIR / 'scripts' / 'OttoRemind.py')]))
     input_button.grid(row=1,column=2,pady=5,padx=5)
     # Otto Sort button
-    input_button = tk.Button(rootHub, text="Sort", command=lambda:subprocess.Popen(['python', './scripts/OttoSort.py']))
+    input_button = tk.Button(rootHub, text="Sort", command=lambda:subprocess.Popen(['python', str(BASE_DIR / 'scripts' / 'OttoSort.py')]))
     input_button.grid(row=2,column=1,pady=5,padx=5)
     # Hour pass button
     toggle_button = tk.Button(rootHub,text="OttoHour OFF",command=toggle_hourpass)
